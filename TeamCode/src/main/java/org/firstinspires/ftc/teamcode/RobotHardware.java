71ab268e9286c7edb49a37f0eaf75c054b094efc
Attack on Robot <32248157+AttackOnRobot@users.noreply.github.com>
2018-11-01 18:03:57 -0500

    Update robotHardware
    added winch and hook servo

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotHardware.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotHardware.java
@@ -3,11 +3,14 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
 
 public abstract class RobotHardware extends LinearOpMode {
     DcMotor leftDriveMotor = null;
     DcMotor rightDriveMotor = null;
-    DcMotor armMotor = null;
+    DcMotor winchMotor = null;
+    Servo hookServo = null;
+   // DcMotor armMotor = null;
 
     protected void configMotor(DcMotor m, DcMotor.Direction d, DcMotor.RunMode runMode) {
         m.resetDeviceConfigurationForOpMode();
@@ -20,11 +23,14 @@ public abstract class RobotHardware extends LinearOpMode {
     protected void initHardware() {
         leftDriveMotor = hardwareMap.dcMotor.get("left");
         rightDriveMotor = hardwareMap.dcMotor.get("right");
-        armMotor = hardwareMap.dcMotor.get("arm");
+        winchMotor = hardwareMap.dcMotor.get("winch");
+        configMotor(winchMotor, DcMotorSimple.Direction.FORWARD, DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        hookServo = hardwareMap.Servo.get("hook");
+        //armMotor = hardwareMap.dcMotor.get("arm");
     }
     protected void setMotorRunMode(DcMotor.RunMode runMode) {
         configMotor(leftDriveMotor, DcMotorSimple.Direction.FORWARD, runMode);
         configMotor(rightDriveMotor, DcMotorSimple.Direction.REVERSE, runMode);
-        configMotor(armMotor, DcMotorSimple.Direction.FORWARD, runMode);
+        //configMotor(armMotor, DcMotorSimple.Direction.FORWARD, runMode);
     }
 }
